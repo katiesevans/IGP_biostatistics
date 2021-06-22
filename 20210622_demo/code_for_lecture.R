3542b6558d5ad08b99cd96e952bd793708e15167
Katie Evans <katiesevans@users.noreply.github.com>
2021-06-21 20:56:43 -0500

    reorg

--- 20210622_demo/code_for_lecture.R
+++ 20210622_demo/code_for_lecture.R
@@ -9,11 +9,18 @@ library(tidyverse)
 # set working directory
 ##### EDIT OR COMMENT OUT ####
 setwd("<working_dir_here>")
-# setwd("~/Dropbox/AndersenLab/LabFolders/Katie/git/IGP_biostatistics/20210622_demo/")
+
+###########################
+#       load data         #
+###########################
 
 # upload data from class github
 income_data <- read.csv("https://raw.githubusercontent.com/katiesevans/IGP_biostatistics/main/20210622_demo/income.data.csv") %>%
     dplyr::select(-X)
+
+###########################
+#   Initial data check    #
+###########################
 
 # check out the data
 summary(income_data$income)
@@ -36,6 +43,10 @@ income_data %>%
     ggplot2::scale_fill_manual(values = c("happiness" = "darkslateblue", "income" = "darkslategray3")) +
     ggplot2::labs(x = "", y = "Frequency")
 ggsave("plots/data_distribution.png", height = 6, width = 10)
+
+###########################
+#    Plot linear model    #
+###########################
 
 # linear model
 lm(happiness ~ income, data = income_data)
@@ -68,6 +79,10 @@ ggplot2::ggsave("plots/happiness_income_plot2.png", height = 6, width = 10)
 plot(income_data$income, income_data$happiness)
 abline(lm(happiness ~ income, data = income_data))
 
+# predict one value - more on prediction below!
+# needs a data frame for a value... if none given, uses the fitted values along the regression line 
+predict(model, data.frame(income = 5))
+
 # plot linear regression model with data and SE
 income_data %>%
     ggplot2::ggplot(.) +
@@ -78,6 +93,7 @@ income_data %>%
           panel.grid = element_blank()) +
     ggplot2::geom_smooth(method = "lm", se = TRUE, formula = y ~ x, color = "red", fill = "red")
 ggplot2::ggsave("plots/happiness_income_plot3.png", height = 6, width = 10)
+ggplot2::ggsave("plots/happiness_income_plot4.png", height = 4, width = 4)
 
 # get more info from the linear model
 summary(lm(happiness ~ income, data = income_data))
@@ -85,20 +101,9 @@ summary(lm(happiness ~ income, data = income_data))
 # predict happiness given set income values
 model <- lm(happiness ~ income, data = income_data)
 
-# seq(1.5, 7.5, 0.1) - produces a vector of 1.5, 1.6, 1.7, 1.8, etc. to use as potential incomes
-predicted_data <- data.frame(income = seq(1.5, 7.5, 0.1)) %>%
-    dplyr::mutate(predicted_happiness = predict(model, newdata = .))
-
-# plot predicted data - should line up with our regression line
-predicted_data %>%
-    ggplot2::ggplot(.) +
-    ggplot2::aes(x = income, y = predicted_happiness) +
-    ggplot2::geom_point() +
-    ggplot2::theme_bw(20)
-ggplot2::ggsave("plots/happiness_predict.png", height = 6, width = 10)
-
-# plot predicted data in base R
-plot(predicted_data$income, predicted_data$predicted_happiness)
+###########################
+#       Residuals         #
+###########################
 
 # plot residual data
 residuals <- income_data %>%
@@ -118,3 +123,38 @@ ggplot2::ggsave("plots/residuals.png", height = 6, width = 10)
 
 # plot residuals base R
 plot(residuals$income, residuals$.resid)
+
+###########################
+#    Regression v. Cor    #
+###########################
+
+# correlation vs. regression
+# regression coefficient = 0.71
+lm(happiness ~ income, data = income_data)
+
+# correlation = 0.865
+cor(income_data$income, income_data$happiness)
+# correlation*correlation = variance explained! (0.75)
+0.8656337^2
+
+###########################
+#     Extra prediction    #
+###########################
+
+# seq(1.5, 7.5, 0.1) - produces a vector of 1.5, 1.6, 1.7, 1.8, etc. to use as potential incomes
+predicted_data <- data.frame(income = seq(1.5, 7.5, 0.1)) %>%
+    dplyr::mutate(predicted_happiness = predict(model, newdata = .))
+
+# plot predicted data - should line up with our regression line
+predicted_data %>%
+    ggplot2::ggplot(.) +
+    ggplot2::aes(x = income, y = predicted_happiness) +
+    ggplot2::geom_point() +
+    ggplot2::theme_bw(20)
+ggplot2::ggsave("plots/happiness_predict.png", height = 6, width = 10)
+
+# plot predicted data in base R
+plot(predicted_data$income, predicted_data$predicted_happiness)
+
+
+
